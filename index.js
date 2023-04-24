c6d59b270da36a647da1fd2620f2f2b4804088c0
idreytser-kryptowire <106192519+idreytser-kryptowire@users.noreply.github.com>
2023-04-24 16:41:04 -0400

    Update index.js
    Updating with requests

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const fs = require("fs");
 const FormData = require("form-data");
-const axios = require("axios").default;
+//const axios = require("axios").default;
+const request = require("request")
 
+/*
 async function run() {
   try {
     // `path-to-file` input defined in action metadata file
@@ -50,5 +52,44 @@ async function run() {
     core.setFailed(error.message);
   }
 }
+*/
 
+async function run() {
+  try {
+    // `path-to-file` input defined in action metadata file
+    const pathToFile = core.getInput("path-to-file");
+    console.log(`Path: ${pathToFile}`);
+    if (fs.existsSync(pathToFile)) {
+      console.log(`File Exists`);
+    } else {
+      console.log(`File Does Not Exist`);
+    }
+
+    // `platform` input defined in action metadata file
+    const platform = core.getInput("platform");
+    console.log(`Platform: ${platform}`);
+
+    // `apiKey` input defined in action metadata file
+    const apiKey = core.getInput("apiKey");
+    console.log("API KEY: " + apiKey );
+
+    const formData = {
+      "app": fs.createReadStream( pathToFile ),
+      "key": apiKey,
+      "platform": platform
+    }
+
+    request.post({url:'https://emm.kryptowire.com/api/submit', formData: formData}, function optionalCallback(err, httpResponse, body) {
+      if (err) {
+        console.error('upload failed:', err);
+      }
+      console.log('Upload successful!  Server responded with:', body);
+      console.log("KryptowireUUID: ", body.uuid);
+    });
+  
+    
+  } catch (err) {
+      console.log("Error with upload:", err);
+  }
+}
 run();
